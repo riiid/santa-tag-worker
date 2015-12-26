61d902a754358f9efeb136db321dca5f777b32bf
chitacan <chitacan@gmail.com>
2015-12-26 21:08:35 +0900

    remove test case.

--- test/validate.js
+++ test/validate.js
@@ -2,7 +2,6 @@ import assert from 'assert';
 import {
   shouldContainCoreTags,
   shouldContainEmpty,
-  malformed$
 } from '../lib/validate';
 
 describe('validate.js', () => {
@@ -54,44 +53,6 @@ describe('validate.js', () => {
     };
     assert.equal(shouldContainEmpty(returnTrue), true);
     assert.equal(shouldContainEmpty(returnFalse), false);
-  });
-
-  it('malformed$ should propagate errored tagInfo', done => {
-    malformed$.onCompleted();
-    malformed$
-      .toArray()
-      .subscribe(
-        res => {
-          assert(res);
-          assert.equal(res.length, 2);
-          assert.deepEqual(res[0], {
-            reason: 'shouldContainCoreTags',
-            publicQnaId: 1234567,
-            coreTag: 'coreTag',
-            tags: [
-              ['tag1', undefined],
-              ['tag2', undefined],
-              ['tag3', undefined],
-              ['tag4', undefined]
-            ]
-          });
-          assert.deepEqual(res[1], {
-            reason: 'shouldContainEmpty',
-            publicQnaId: 1234567,
-            coreTag: 'coreTag',
-            tags: [
-              ['tag1', 'tag5'],
-              ['tag2', 'tag6'],
-              ['tag4', undefined],
-              ['tag4', undefined]
-            ]
-          });
-        },
-        err => {
-          throw err;
-        },
-        () => done()
-      );
   });
 });
 
