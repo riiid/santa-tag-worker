4d562a13aa4defd325763aa9c5b8b1c1fa6b8532
chitacan <chitacan@gmail.com>
2015-12-30 16:09:38 +0900

    add shouldContainSingleCoreTag function and its test case.

--- test/validate.js
+++ test/validate.js
@@ -5,7 +5,8 @@ import {
   shouldHaveCoreTag,
   shouldNotContainReason,
   shouldContainCoreTags,
-  shouldContainEmpty
+  shouldContainEmpty,
+  shouldContainSingleCoreTag
 } from '../lib/validate';
 
 describe('validate.js', () => {
@@ -169,5 +170,60 @@ describe('validate.js', () => {
     assert.equal(shouldContainEmpty(returnTrue), true);
     assert.equal(shouldContainEmpty(returnFalse), false);
   });
-});
 
+  it('shouldContainSingleCoreTag', () => {
+    const returnTrue = {
+      publicQnaId: 1234567,
+      coreTag: {id: 1, name: 'coreTag'},
+      wrong_answers: [
+        {
+          number: 1,
+          answer: false,
+          wrong_answer_tags: [
+            {id: null, tag: {id: 1, name: 'coreTag'}},
+            {id: null, tag: {id: 5, name: 'tag5'}}
+          ]
+        },
+        {
+          number: 2,
+          answer: false,
+          wrong_answer_tags: [
+            {id: null, tag: {id: 2, name: 'tag2'}},
+            {id: null, tag: {id: 6, name: 'tag6'}}
+          ]
+        },
+        {
+          number: 3,
+          answer: false,
+          wrong_answer_tags: [
+            {id: null, tag: {id: 1, name: 'coreTag'}}
+          ]
+        },
+      ]
+    };
+    const returnFalse = {
+      publicQnaId: 1234567,
+      coreTag: {id: 1, name: 'coreTag'},
+      wrong_answers: [
+        {
+          number: 1,
+          answer: false,
+          wrong_answer_tags: [
+            {id: null, tag: {id: 1, name: 'coreTag'}},
+            {id: null, tag: {id: 5, name: 'tag5'}}
+          ]
+        },
+        {
+          number: 2,
+          answer: false,
+          wrong_answer_tags: [
+            {id: null, tag: {id: 2, name: 'tag2'}},
+            {id: null, tag: {id: 6, name: 'tag6'}}
+          ]
+        }
+      ]
+    };
+    assert.equal(shouldContainSingleCoreTag(returnTrue), true);
+    assert.equal(shouldContainSingleCoreTag(returnFalse), false);
+  });
+});
